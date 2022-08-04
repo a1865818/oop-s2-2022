6a045d5ceb7180a035516815013545ec80f5e0c9
minhnt10122003 <109890584+a1865818@users.noreply.github.com>
2022-08-04 14:11:27 +0930

    add

--- function-1-5.cpp
+++ function-1-5.cpp
@@ -6,7 +6,7 @@ void print_summed(int array1[3][3], int array2[3][3]) {
     for (int i = 0; i < row1; i++) {
       for (int j = 0; j < col1; j++) {
         if (j == col1 - 1) {
-          cout << (array1[i][j] + array2[i][j]) << " ";
+          cout << (array1[i][j] + array2[i][j]) ;
         } else {
           cout << (array1[i][j] + array2[i][j]) << " ";
         }
