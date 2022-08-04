077d731fa84a400b25f762bc4f603e314b1ca862
minhnt10122003 <109890584+a1865818@users.noreply.github.com>
2022-08-04 14:22:53 +0930

    add

--- function-1-4.cpp
+++ function-1-4.cpp
@@ -1,17 +1,10 @@
 #include <iostream>
 using namespace std;
- void print_scaled(int array[3][3],int scale) {
-    for (int i = 0; i < 3; i++)
-    {
-        for (int j = 0; j < 3; j++)
-        {
-            if ( j == 2) {
-                cout << array[i][j]*3;
-            } else {
-                cout<<array[i][j]*3<<" ";
-            }
-        }
-        cout<<endl;
+void print_scaled(int array[3][3], int scale) {
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 3; j++) {
+      cout << array[i][j] * 3 << " " ;
     }
-    
- }
+    cout << endl;
+  }
+}
