f8ce4849ce5938c50cd641efe89bf396082810bc
minhnt10122003 <109890584+a1865818@users.noreply.github.com>
2022-08-08 13:29:28 +0930

    add

--- function-1-2.cpp
+++ function-1-2.cpp
@@ -3,7 +3,7 @@ using namespace std;
 void print_class(std::string courses[4], std::string students[],
                  int report_card[][4], int nstudents) {
   cout
-       << "Report Card";
+       << "Report Card"<" ";
   for (int i = 0; i < 4; i++) {
     if (i != 4) {
       cout << courses[i] << " ";
