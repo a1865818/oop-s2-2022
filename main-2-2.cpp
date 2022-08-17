8022bbd52b0da59e62942eee2bbb66438b2b7e25
minhnt10122003 <109890584+a1865818@users.noreply.github.com>
2022-08-17 15:12:57 +0930

    add

--- main-2-2.cpp
+++ main-2-2.cpp
@@ -5,6 +5,6 @@ extern int max_sub_sum(int *nums,int length) ;
 int main () {
     int old_array[10] = { 31,-41,59,26,-53,58,97,-93,-23,-84 };
     int *ptr = &old_array[0];
-cout  << max_sub_sum(ptr, 10) << endl;
+cout  << max_sub_sum(ptr, 10) ;
 return 0;
 }
