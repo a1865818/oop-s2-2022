1fb4e35e211ebb64b5710b348c6a068a2afc46fb
minhnt10122003 <109890584+a1865818@users.noreply.github.com>
2022-08-17 15:17:29 +0930

    add

--- function-2-2.cpp
+++ function-2-2.cpp
@@ -6,7 +6,7 @@ int max_sub_sum(int *nums,int length) {
  int sum = 0;
 int max_so_far = -1, max_ending_here = 0;
 
-for (int i = 0; i < length; i++) {
+for (int i = 0; i < length-1; i++) {
 sum = sum + *nums;;
 if (max_so_far < sum)
 max_so_far = sum;
