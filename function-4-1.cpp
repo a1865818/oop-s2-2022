0b3c079048d2b3ee6b78ed7bd66fc3429889c382
minhnt10122003 <109890584+a1865818@users.noreply.github.com>
2022-08-25 14:09:55 +0930

    add

--- function-4-1.cpp
+++ function-4-1.cpp
@@ -1,30 +1,21 @@
 #include <iostream>
-using namespace std;
-int *matrix_min_max(int **vals, int num_rows, int num_cols) {
-  int *mm = new int[2];
-  
-  int min = vals[0][0];
-  int max = vals[0][0];
-  for (int i = 0; i < num_rows; i++) {
-    for (int j = 0; j < num_cols; j++) {
-      if (vals[i][j] >= max)
-        max = vals[i][j];
-      else if (vals[i][j] <= min)
-        min = vals[i][j];
-    }
-  }
-  for (int i = 0; i < 2; i++)
-  {
-    mm[i]=min;
-    if (i=1)
-    {
-      mm[i]=max;
-    }
-    
-  }
-  
-  cout <<"min: " <<*mm <<" " << "max: "<<*(mm+1 )<<endl;
+#include <stdlib.h>
 
-  
-  return 0;
+using namespace std;
+int* matrix_min_max(int **vals, int row, int col){
+    int min = **vals;
+    int max = **vals;
+    int *ret = new int(2);
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j ++) {
+            if (min > *(*(vals+i)+j)) {
+                min = *(*(vals+i)+j);
+            } else if (max < *(*(vals+i)+j)) {
+                max = *(*(vals+i)+j);
+            }
+        }   
+    }
+    *ret = min;
+    *(ret+1) = max;
+    return ret;
 }
--- main-4-1.cpp
+++ main-4-1.cpp
@@ -6,7 +6,8 @@ int main () {
     int row2[]={100,5,73};
     int row3[]={23,56,1};
     int *vals[]={row1,row2,row3};
-    matrix_min_max(vals,3,3);
+   int *mm=matrix_min_max(vals,3,3);
+   cout<<*mm;
     return 0;
 }
 
