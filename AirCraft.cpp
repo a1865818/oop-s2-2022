436520ea594d9528ca844d3c30210c6083dd8bfd
minhnt10122003 <109890584+a1865818@users.noreply.github.com>
2022-10-28 17:21:21 +1030

    add

--- AirCraft.cpp
+++ AirCraft.cpp
@@ -1,33 +1,39 @@
-#include"AirCraft.h"
-#include<iostream>
-#include<string>
-AirCraft::AirCraft() {
+#include "AirCraft.h"
+
+AirCraft::AirCraft(){
 
 }
-AirCraft::AirCraft(int w) {
-    weight=w;
+
+AirCraft::AirCraft(int w){
+    weight = w;
 }
-void AirCraft::set_fuel(float f){
-    fuel=f;
-}
-void AirCraft::set_numflight(int nflight){
-    numberOfFlights=nflight;
-}
-void AirCraft::set_weight(int w){
-    weight=w;
-}
-void AirCraft::refuel() {
-    fuel=100;
-}
-float AirCraft::get_fuel(){
-    return fuel;
-}
-int AirCraft::get_numflight() {
-    return numberOfFlights;
-}
+
 int AirCraft::get_weight(){
     return weight;
 }
-AirCraft::~AirCraft() {
-    
+
+void AirCraft::set_weight(int w){
+    weight = w;
 }
+
+float AirCraft::get_fuel(){
+    return fuel;
+}
+
+void AirCraft::set_fuel(float f){
+    fuel = f;
+}
+
+int AirCraft::get_numberOfFlights(){
+    return numberOfFlights;
+}
+
+void AirCraft::set_numberOfFlights(int n){
+    numberOfFlights = n;
+}
+
+void AirCraft::refuel(){
+    fuel = 100;
+}
+
+AirCraft::~AirCraft(){}
--- AirCraft.h
+++ AirCraft.h
@@ -1,24 +1,27 @@
 #ifndef AIRCRAFT_H
 #define AIRCRAFT_H
 #include <iostream>
-#include <string>
-using namespace std;
+
 class AirCraft {
  protected:
-      int weight;           // the weight of AirCraft
-      float fuel=100;           // fuel percentage, initially 100%
-      int numberOfFlights=0;  // initially 0
-    public:
-        AirCraft();
-        AirCraft(int w);// creates an Aircraft with weight w
-        void refuel();               // Resets fuel back to 100%
-        void set_weight(int w);
-        void set_numflight(int nflight);
-        void set_fuel(float f);
-        float get_fuel();
-        int get_weight();
-        int get_numflight();
-        virtual void fly(int headwind, int minutes)=0;         // headwind in km/h and minutes (time flying)
-        ~AirCraft();
+  int weight;               // the weight of AirCraft
+  float fuel = 100;         // fuel percentage, initially 100%
+  int numberOfFlights = 0;  // number of flights
+ public:
+  AirCraft();
+  AirCraft(int w);
+  void fly(int headwind, int minutes);
+  void refuel();  // refuel the aircraft
+  void set_numberOfFlights(int n);
+  void set_fuel(float f);
+  void set_weight(int w);
+
+  float get_fuel();
+
+  int get_weight();
+  int get_numberOfFlights();
+
+  ~AirCraft();
 };
+
 #endif
