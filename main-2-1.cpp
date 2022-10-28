db35ce18118172ae8768888cea71520fa8b08604
minhnt10122003 <109890584+a1865818@users.noreply.github.com>
2022-10-28 17:16:13 +1030

    add

--- main-2-1.cpp
+++ main-2-1.cpp
@@ -3,9 +3,6 @@
 #include<iostream>
 #include<string>
 int main () {
-    Helicopter *heli1;
-    heli1=new Helicopter(5690,"AB");
-    heli1->fly(45,10);
-    cout<<heli1->get_fuel()<<" "<<heli1->get_numflight()<<endl;
+
     return 0;
 }
